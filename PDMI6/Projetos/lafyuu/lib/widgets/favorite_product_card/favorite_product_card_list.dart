55efe48949a8c56c791254f643dc33bc44916094
paulaandrezza <paulaandrezza25@gmail.com>
2025-05-19 15:07:26 -0300

    adjust landscape

--- PDMI6/Projetos/lafyuu/lib/widgets/favorite_product_card/favorite_product_card_list.dart
+++ PDMI6/Projetos/lafyuu/lib/widgets/favorite_product_card/favorite_product_card_list.dart
@@ -22,7 +22,7 @@ class FavoriteProductCardList extends StatelessWidget {
         crossAxisCount: isLandscape ? 4 : 2,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
-        childAspectRatio: isLandscape ? 0.65 : 0.55,
+        childAspectRatio: isLandscape ? 0.60 : 0.55,
       ),
       itemCount: favoriteProducts.length,
       itemBuilder: (context, index) {
