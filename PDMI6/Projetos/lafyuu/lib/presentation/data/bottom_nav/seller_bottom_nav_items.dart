e523def898889c2cb00e7a9340fae06093a822da
paulaandrezza <paulaandrezza25@gmail.com>
2025-06-24 12:14:39 -0300

    fix navigation bar error

--- PDMI6/Projetos/lafyuu/lib/presentation/data/bottom_nav/seller_bottom_nav_items.dart
+++ PDMI6/Projetos/lafyuu/lib/presentation/data/bottom_nav/seller_bottom_nav_items.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:lafyuu/presentation/models/bottom_nav_item_data.dart';
+import 'package:lafyuu/views/main/client/tabScreens/account/account_screen.dart';
 import 'package:lafyuu/views/main/seller/tabScreens/productManager/product_manager_screen.dart';
 
 final List<BottomNavItemData> sellerBottomNavItems = [
@@ -7,5 +8,10 @@ final List<BottomNavItemData> sellerBottomNavItems = [
     icon: Icons.inventory_2_outlined,
     label: 'Products',
     screen: const ProductManagerScreen(),
+  ),
+  BottomNavItemData(
+    icon: Icons.person_outlined,
+    label: 'Account',
+    screen: AccountScreen(),
   ),
 ];
